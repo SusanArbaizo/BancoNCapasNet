8b03bdcb1a75d54a6d9c4d5430b1a7d551e80f66
SusanArbaizo <susanarbaizodelacruz2@gmail.com>
2023-02-17 21:50:51 -0500

    Se agregó las entidades Cliente, TipoCliente y TipoDocumento

---
+++ Banco.Entidades/TipoCliente.cs
@@ -0,0 +1,11 @@
+﻿namespace Banco.Entidades
+{
+    public class TipoCliente
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+
+    }
+}
