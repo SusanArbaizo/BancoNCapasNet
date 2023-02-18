3739f7bd8c57fb5e2a80c712bba5241a0aaa8149
SusanArbaizo <susanarbaizodelacruz2@gmail.com>
2023-02-17 21:59:48 -0500

    Se agrego las entidades

--- Banco.Entidades/Cliente.cs
+++ Banco.Entidades/Cliente.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Banco.Entidades
 {
-    internal class Cliente
+    public class Cliente
     {
+        public int ID { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string RazonSocial { get; set; }
+        public string NumeroDocumento { get; set; }
+        public int IdTipoCliente { get; set; }
+        public int IdTipoDocumento { get; set; }
+        public string Direccion { get; set; }
+        public string  Referencia { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
     }
 }
